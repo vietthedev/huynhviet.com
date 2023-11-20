5f92c8f76b975b45ea3fcedacf381a79a5c2e867
Viet Huynh <xa.lach@yahoo.com>
2023-11-20 19:15:58 +0700

    fix: check props.class undefined

--- components/Typography/Paragraph.tsx
+++ components/Typography/Paragraph.tsx
@@ -1,7 +1,7 @@
 import { JSX } from "preact/jsx-runtime";
 
 const Paragraph = (props: JSX.HTMLAttributes<HTMLParagraphElement>) => {
-  return <p {...props} class={`my-4 ${props.class}`} />;
+  return <p {...props} class={`my-4 ${props.class ?? ""}`} />;
 };
 
 export default Paragraph;
