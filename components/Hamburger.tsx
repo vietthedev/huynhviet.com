77d3e331cdaa6e946a4d07b31e04c338047568e4
Viet Huynh <xa.lach@yahoo.com>
2023-11-25 19:13:07 +0700

    feat: hamburger menu for mobile devices

---
+++ components/Hamburger.tsx
@@ -0,0 +1,68 @@
+import { ComponentChildren } from "preact";
+
+interface HamburgerProps {
+  children: ComponentChildren;
+}
+
+const Hamburger = (props: HamburgerProps) => {
+  const { children } = props;
+
+  return (
+    <label
+      class="lg:hidden relative z-40 cursor-pointer px-3 py-6 [&_svg]:z-50 [&_svg]:relative"
+      for="mobile-menu"
+    >
+      <input class="peer hidden" type="checkbox" id="mobile-menu" />
+      <svg
+        class="peer-checked:hidden fill-none stroke-current stroke-[1.2px]"
+        width="21"
+        height="14"
+        viewBox="0 0 21 14"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <line
+          x1="0.25"
+          y1="1.4"
+          x2="20.25"
+          y2="1.4"
+        >
+        </line>
+        <line
+          x1="0.25"
+          y1="7.4"
+          x2="20.25"
+          y2="7.4"
+        >
+        </line>
+        <line
+          x1="0.25"
+          y1="13.4"
+          x2="10.25"
+          y2="13.4"
+        >
+        </line>
+      </svg>
+      <svg
+        class="h-6 w-6 hidden peer-checked:block fill-none stroke-current stroke-2"
+        viewBox="0 0 24 24"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M6 18L18 6M6 6l12 12"
+        >
+        </path>
+      </svg>
+      <div class="fixed inset-0 z-40 hidden h-full w-full bg-black/50 backdrop-blur-sm peer-checked:block">
+        &nbsp;
+      </div>
+      <div class="fixed top-0 left-0 z-40 h-full w-full -translate-x-full overflow-y-auto overscroll-y-none transition duration-500 peer-checked:translate-x-0">
+        <div class="float-left min-h-full w-[85%] bg-white px-6 pt-12 shadow-2xl">
+          {children}
+        </div>
+      </div>
+    </label>
+  );
+};
+
+export default Hamburger;
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,7 @@
 import Link from "@/components/Link.tsx";
 import List from "@/components/List.tsx";
+import Hamburger from "@/components/Hamburger.tsx";
+import Title from "@/components/Typography/Title.tsx";
 
 const menus = [
   { href: "/", name: "Home" },
@@ -9,15 +11,15 @@ const menus = [
 const Header = () => {
   return (
     <header class="border-b border-gray-300">
-      <nav class="py-6 px-8 md:px-32 flex flex-col md:flex-row gap-4">
+      <nav class="py-6 px-8 md:px-32 flex items-center md:flex-row gap-4">
         <div class="flex items-center flex-1">
           <Link class="text-3xl font-bold hover:!no-underline" href="/">
             Việt Huỳnh
           </Link>
         </div>
-        <List class="flex items-center gap-6 list-none">
+        <List class="hidden lg:flex items-center gap-6 list-none">
           {menus.map((menu) => (
-            <List.Item key={menu.name} class="ml-0 mt-0">
+            <List.Item key={menu.name} class="!ml-0 !mt-0">
               <Link
                 href={menu.href}
                 class="py-1 data-[current]:font-bold"
@@ -27,6 +29,26 @@ const Header = () => {
             </List.Item>
           ))}
         </List>
+
+        <Hamburger>
+          <Link class="text-3xl font-bold hover:!no-underline" href="/">
+            <Title>
+              Việt Huỳnh
+            </Title>
+          </Link>
+          <List class="list-none">
+            {menus.map((menu) => (
+              <List.Item key={menu.name}>
+                <Link
+                  href={menu.href}
+                  class="py-1 data-[current]:font-bold"
+                >
+                  {menu.name}
+                </Link>
+              </List.Item>
+            ))}
+          </List>
+        </Hamburger>
       </nav>
     </header>
   );
