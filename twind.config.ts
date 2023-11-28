c09f70cf2f5addef406d7836676b15ce26b5d88e
Viet Huynh <xa.lach@yahoo.com>
2023-11-28 16:17:13 +0700

    feat: dark mode support

--- twind.config.ts
+++ twind.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig, Preset } from "@twind/core";
+import { autoDarkColor, defineConfig, Preset } from "@twind/core";
 import presetAutoprefix from "@twind/preset-autoprefix";
 import presetTailwind from "@twind/preset-tailwind";
 import presetTypography, { TypographyTheme } from "@twind/preset-typography";
 
 export default {
   ...defineConfig({
+    darkColor: autoDarkColor,
     presets: [
       presetTypography() as Preset<TypographyTheme>,
       presetTailwind(),
