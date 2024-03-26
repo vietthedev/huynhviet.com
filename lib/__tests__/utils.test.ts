4439f3845e2300a353a78f24c6331df8793c16c9
Viet Huynh <xa.lach@yahoo.com>
2024-03-26 19:12:39 +0700

    update unit tests to use bdd

--- lib/__tests__/utils.test.ts
+++ lib/__tests__/utils.test.ts
@@ -1,31 +1,37 @@
 import { assert, assertEquals, assertExists } from "$std/assert/mod.ts";
+import { describe, it } from "$std/testing/bdd.ts";
 import { getPost, getPosts } from "@/lib/utils.ts";
 
-Deno.test(`${getPosts.name} - private posts included`, async () => {
-  const posts = await getPosts(true);
+describe(getPosts.name, () => {
+  it("should include private posts", async () => {
+    const posts = await getPosts(true);
 
-  assert(
-    posts.some((post) => post.slug === "2017-06-28-hom-nay-la-mot-ngay-buon"),
-  );
-  assert;
+    assert(
+      posts.some((post) => post.slug === "2017-06-28-hom-nay-la-mot-ngay-buon"),
+    );
+  });
+
+  it("should not include private posts", async () => {
+    const posts = await getPosts();
+
+    assert(
+      posts.every((post) =>
+        post.slug !== "2017-06-28-hom-nay-la-mot-ngay-buon"
+      ),
+    );
+  });
 });
 
-Deno.test(`${getPosts.name} - private posts excluded`, async () => {
-  const posts = await getPosts();
+describe(getPost.name, () => {
+  it("should return existing post", async () => {
+    const post = await getPost("2015-11-08-the-beginning-of-a-journey");
 
-  assert(
-    posts.every((post) => post.slug !== "2017-06-28-hom-nay-la-mot-ngay-buon"),
-  );
-});
+    assertExists(post);
+  });
 
-Deno.test(`${getPost.name} - post exists`, async () => {
-  const post = await getPost("2015-11-08-the-beginning-of-a-journey");
+  it("should return null for non-existing post", async () => {
+    const post = await getPost("something-not-existing");
 
-  assertExists(post);
-});
-
-Deno.test(`${getPost.name} - post doesn't exist`, async () => {
-  const post = await getPost("something-not-existing");
-
-  assertEquals(post, null);
+    assertEquals(post, null);
+  });
 });
